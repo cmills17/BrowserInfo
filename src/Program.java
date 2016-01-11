0cfa658c650d9265a54ee43a411977842df07ecc
Phillip Chung <pchung@deerfield.edu>
2016-01-10 21:05:08 -0500

    Update

--- src/Program.java
+++ src/Program.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+
+
+
 public class Program {
 	
 	JTextField computercost = new JTextField();
@@ -11,11 +14,11 @@ public class Program {
 	JComboBox laptoptype = new JComboBox(laptoptype_choices);
 	JLabel laptoptype_q = new JLabel ("Laptop type");
 
-	String[] screensize_choices = {"11","13","14","15","16","17"};
+	String[] screensize_choices = {"15''", "17''","19''", "20''","21''","22''","24''","30''","32''","37''","42''","50''"};
 	JComboBox screensize = new JComboBox(screensize_choices);
 	JLabel screensize_q = new JLabel ("Screen size");
 
-	String[] corenumber_choices = {"1","2","3","4","5","6"};
+	String[] corenumber_choices = {"1 Core","2 Cores","3 Cores","4 Cores","5 Cores","6 Cores","7 Cores","8 Cores"};
 	JComboBox corenumber = new JComboBox(corenumber_choices);
 	JLabel corenumber_q = new JLabel ("Number of cores");
 
@@ -27,10 +30,22 @@ public class Program {
 	JComboBox ramTypes = new JComboBox(ramTypes_choices);
 	JLabel ramTypes_q = new JLabel ("RAM type");
 
+    String[] hardDrive_choices = {"Solid State Drive SSD","2.5'' Hard Disk Drive HHD","3.5'' Hard Disk Drive HHD" };
+	JComboBox hardDrive = new JComboBox(hardDrive_choices);
+	JLabel hardDrive_q = new JLabel ("Hard Drive");
 
+    String[] state_choices = {"CA","CT","D.C.","FL","IL","MA","NH","NJ","NC","TX","United States","Canada","Hong Kong","India","Mexico","Singapore","United Kingdom" };
+	JComboBox state = new JComboBox(state_choices);
+	JLabel state_q = new JLabel ("Where do you live?");
 
-	JButton b = new JButton("Calculate");
-	JLabel l = new JLabel ("Display Combo items");
+	JTextField computerKeep = new JTextField();
+	JLabel computerKeep_q = new JLabel ("How long you expect to keep the computer?");
+
+	JTextField powerOn = new JTextField();
+	JLabel powerOn_q = new JLabel ("How many hours a day do you leave the power on?");
+	
+	JButton calculate = new JButton("Calculate");
+	JLabel costcalculations = new JLabel ("");
 	
 	
 
@@ -44,7 +59,7 @@ public void frame() {
 	
 	JFrame f = new JFrame();
 	f.setVisible(true);
-	f.setSize(600,600);
+	f.setSize(700,500);
 	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
 	JPanel p = new JPanel(new GridLayout(0,2));
@@ -60,27 +75,119 @@ public void frame() {
 	p.add(brandOfCPU);
 	p.add(ramTypes_q);
 	p.add(ramTypes);
+	p.add(hardDrive_q);
+	p.add(hardDrive);
+	p.add(state_q);
+	p.add(state);
+	p.add(computerKeep_q);
+	p.add(computerKeep);
+	p.add(powerOn_q);
+	p.add(powerOn);
 
 
 
-
-
-	p.add(b);
-	p.add(l);
+	p.add(calculate);
+	p.add(costcalculations);
 
 
 	
 	f.add(p);
 
 
+	calculate.addActionListener(new ActionListener () {
+		public void actionPerformed (ActionEvent e) {
+			String laptoptype_s = laptoptype.getSelectedItem().toString();
+			String screensize_s = screensize.getSelectedItem().toString();
+			String corenumber_s = corenumber.getSelectedItem().toString();
+			String brandOfCPU_s = brandOfCPU.getSelectedItem().toString();
+			String ramTypes_s = ramTypes.getSelectedItem().toString();
+			String hardDrive_s = hardDrive.getSelectedItem().toString();
+			
+			double watts = 0;
+			if (laptoptype_s.equals("Desktop")){
+				watts = watts + 120;}
+			if (laptoptype_s.equals("Laptop")){
+				watts = watts + 35;}
+			if (screensize_s.equals("15''")){
+				watts = watts + 18;}
+			if (screensize_s.equals("17''")){
+				watts = watts + 20;}
+			if (screensize_s.equals("19''")){
+				watts = watts + 22;}
+			if (screensize_s.equals("20''")){
+				watts = watts + 26;}
+			if (screensize_s.equals("21''")){
+				watts = watts + 30;}
+			if (screensize_s.equals("22''")){
+				watts = watts + 40;}
+			if (screensize_s.equals("24''")){
+				watts = watts + 50;}
+			if (screensize_s.equals("30''")){
+				watts = watts + 60;}
+			if (screensize_s.equals("32''")){
+				watts = watts + 70;}
+			if (screensize_s.equals("37''")){
+				watts = watts + 80;}
+			if (screensize_s.equals("42''")){
+				watts = watts + 120;}
+			if (screensize_s.equals("50''")){
+				watts = watts + 150;}
+			if (corenumber_s.equals("1 Core")){
+				watts = watts + 69;}
+			if (corenumber_s.equals("2 Cores")){
+				watts = watts + 74.5;}
+			if (corenumber_s.equals("3 Cores")){
+				watts = watts + 80;}
+			if (corenumber_s.equals("4 Cores")){
+				watts = watts + 86;}
+			if (corenumber_s.equals("5 Cores")){
+				watts = watts + 86;}
+			if (corenumber_s.equals("6 Cores")){
+				watts = watts + 86.5;}
+			if (corenumber_s.equals("7 Cores")){
+				watts = watts + 86.5;}
+			if (corenumber_s.equals("8 Cores")){
+				watts = watts + 86.5;}
+			if (brandOfCPU_s.equals("Intel Low End")){
+				watts = watts + 64;}
+			if (brandOfCPU_s.equals("Intel Mid End")){
+				watts = watts + 84;}
+			if (brandOfCPU_s.equals("Intel High End")){
+				watts = watts + 86;}
+			if (brandOfCPU_s.equals("Intel Top End")){
+				watts = watts + 104;}
+			if (brandOfCPU_s.equals("AMD Low End")){
+				watts = watts + 80;}
+			if (brandOfCPU_s.equals("AMD Mid End")){
+				watts = watts + 95;}
+			if (brandOfCPU_s.equals("AMD High End")){
+				watts = watts + 110;}
+			if (ramTypes_s.equals("DDR1 RAM")){
+				watts = watts + 4.75;}
+			if (ramTypes_s.equals("DDR2 RAM")){
+				watts = watts + 3.75;}
+			if (ramTypes_s.equals("DDR3 RAM")){
+				watts = watts + 2.5;}
+			if (hardDrive_s.equals("Solid State Drive SSD")){
+				watts = watts + 1.7;}
+			if (hardDrive_s.equals("2.5'' Hard Disk Drive HHD")){
+				watts = watts + 1.85;}
+			if (hardDrive_s.equals("3.5'' Hard Disk Drive HHD")){
+				watts = watts + 7.75;}
+			
+			System.out.println(watts);
+			
+		}
+	});
 	
 }
 	
 
 public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 	new Program();
 	}
-
 }
+
+//String computercost_s = computercost.getText();
+//costcalculations.setText(computercost_s);

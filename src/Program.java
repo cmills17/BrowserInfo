5faf043dade14d0c2d05465610c24e17f15a2d5f
Phillip Chung <pchung@deerfield.edu>
2016-01-10 23:35:28 -0500

    Almost final product.

--- src/Program.java
+++ src/Program.java
@@ -3,16 +3,14 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-
-
 public class Program {
 	
 	JTextField computercost = new JTextField();
-	JLabel computercost_q = new JLabel ("What is the cost of the computer?");
+	JLabel computercost_q = new JLabel ("What is the cost of the computer? (dollars)");
 
 	String[] laptoptype_choices = {"Laptop","Desktop"};
 	JComboBox laptoptype = new JComboBox(laptoptype_choices);
-	JLabel laptoptype_q = new JLabel ("Laptop type");
+	JLabel laptoptype_q = new JLabel ("Computer type");
 
 	String[] screensize_choices = {"15''", "17''","19''", "20''","21''","22''","24''","30''","32''","37''","42''","50''"};
 	JComboBox screensize = new JComboBox(screensize_choices);
@@ -39,16 +37,20 @@ public class Program {
 	JLabel state_q = new JLabel ("Where do you live?");
 
 	JTextField computerKeep = new JTextField();
-	JLabel computerKeep_q = new JLabel ("How long you expect to keep the computer?");
+	JLabel computerKeep_q = new JLabel ("How long do you expect to keep the computer? (years)");
 
 	JTextField powerOn = new JTextField();
 	JLabel powerOn_q = new JLabel ("How many hours a day do you leave the power on?");
 	
 	JButton calculate = new JButton("Calculate");
-	JLabel costcalculations = new JLabel ("");
 	
-	
+	JLabel emptyspace = new JLabel ("");
+	JLabel computer_costcalculations = new JLabel ("");
+	JLabel electricity_costcalculations = new JLabel ("");
+	JLabel battery_costcalculations = new JLabel ("");
+	JLabel total_costcalculations = new JLabel ("");
 
+	
 	
 	public Program() {
 		
@@ -84,15 +86,14 @@ public void frame() {
 	p.add(powerOn_q);
 	p.add(powerOn);
 
-
-
 	p.add(calculate);
-	p.add(costcalculations);
+	p.add(emptyspace);
+	p.add(computer_costcalculations);
+	p.add(electricity_costcalculations);
+	p.add(battery_costcalculations);
+	p.add(total_costcalculations);
 
-
-	
 	f.add(p);
-
 
 	calculate.addActionListener(new ActionListener () {
 		public void actionPerformed (ActionEvent e) {
@@ -102,8 +103,21 @@ public void frame() {
 			String brandOfCPU_s = brandOfCPU.getSelectedItem().toString();
 			String ramTypes_s = ramTypes.getSelectedItem().toString();
 			String hardDrive_s = hardDrive.getSelectedItem().toString();
+			String state_s = state.getSelectedItem().toString();
+			
+			String computercost_s = computercost.getText();			
+			double computercost_n = Double.parseDouble(computercost_s);
+			computercost_n = ((int)(computercost_n*100))/100.0;
+
+			String powerOn_s = powerOn.getText();			
+			double powerOn_n = Double.parseDouble(powerOn_s);
+			
+			String computerKeep_s = computerKeep.getText();			
+			double computerKeep_n = Double.parseDouble(computerKeep_s);
 			
 			double watts = 0;
+			double region = 0;
+
 			if (laptoptype_s.equals("Desktop")){
 				watts = watts + 120;}
 			if (laptoptype_s.equals("Laptop")){
@@ -175,8 +189,59 @@ public void frame() {
 			if (hardDrive_s.equals("3.5'' Hard Disk Drive HHD")){
 				watts = watts + 7.75;}
 			
-			System.out.println(watts);
+			if (state_s.equals("CA")){
+				region = 0.135;}
+			if (state_s.equals("CT")){
+				region = 0.155;}
+			if (state_s.equals("D.C.")){
+				region = 0.119;}
+			if (state_s.equals("FL")){
+				region = 0.104;}
+			if (state_s.equals("IL")){
+				region = 0.084;}
+			if (state_s.equals("MA")){
+				region = 0.138;}
+			if (state_s.equals("NH")){
+				region = 0.142;}
+			if (state_s.equals("NJ")){
+				region = 0.137;}
+			if (state_s.equals("NC")){
+				region = 0.0915;}
+			if (state_s.equals("TX")){
+				region = 0.0855;}
+			if (state_s.equals("USA")){
+				region = 0.0984;}
+			if (state_s.equals("Canada")){
+				region = 0.0905;}
+			if (state_s.equals("Hong Kong")){
+				region = 0.1801;}
+			if (state_s.equals("India")){
+				region = 0.1;}
+			if (state_s.equals("Mexico")){
+				region = 0.1928;}
+			if (state_s.equals("Singapore")){
+				region = 0.2153;}
+			if (state_s.equals("United Kingdom")){
+				region = 0.2;}
 			
+			double electricityCost = (watts/1000)*powerOn_n*computerKeep_n*365*region;
+			electricityCost = ((int)(electricityCost*100))/100.0;
+			double totalCost = computercost_n + electricityCost;
+			totalCost = ((int)(totalCost*100))/100.0;
+			
+			double batteryCost= 0;
+			if (laptoptype.equals("Desktop")){
+				batteryCost = ((Math.ceil(computerKeep_n/4))-1)*7;}
+			if (laptoptype.equals("Laptop")){
+				batteryCost = ((Math.ceil(computerKeep_n/3))-1)*275;}
+			
+			
+			computer_costcalculations.setText("Computer Cost: $" + computercost_n);
+			electricity_costcalculations.setText("Electricity Cost: $" + electricityCost);
+			battery_costcalculations.setText("Battery Cost: $" + batteryCost);
+			total_costcalculations.setFont(new Font("Calibri", Font.BOLD, 16));
+			total_costcalculations.setText("Total Cost: $" + totalCost);
+
 		}
 	});
 	
@@ -188,6 +253,3 @@ public static void main(String[] args)
 	new Program();
 	}
 }
-
-//String computercost_s = computercost.getText();
-//costcalculations.setText(computercost_s);
